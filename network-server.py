2b2c027fc69ab6bcbcf042376bd2e1a6c46f0dcd
Hans KRC4267 <krc4267@gmail.com>
2016-06-03 22:16:42 -0400

    We now have a server that runs in loops, not exits after getting a message

--- network-server.py
+++ network-server.py
@@ -3,19 +3,19 @@
 import socket
 
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_IP = '192.168.1.78'
+TCP_PORT = 42626
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-
-conn, addr = s.accept()
-print('Connection address:', addr)
 while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print("received data:", data)
-    conn.send(data)  # echo
-conn.close()
+	conn, addr = s.accept()
+	print('Connection address:', addr)
+	while 1:
+		data = conn.recv(BUFFER_SIZE)
+		if not data: break
+		print("received data:", data)
+		conn.send(data)  # echo
+	conn.close()
