fd693a0b84e28900486ff51cc5ac3ff14fbf1e07
Hans KRC4267 <krc4267@gmail.com>
2016-06-04 15:11:29 -0400

    Gots Things.

--- network-client.2.py
+++ network-client.2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
 TCP_IP = input('Input the IP to connect to: ')
 TCP_PORT = 42626
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 MESSAGE = "Hello, World!"
 
 while 1:
@@ -12,6 +13,6 @@ while 1:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((TCP_IP, TCP_PORT))
 	s.send(MESSAGE.encode('utf-8'))
+	time.sleep(1)
 	data = s.recv(BUFFER_SIZE)
-	s.close()
 	print("received data:", data)
--- network-server.py
+++ network-server.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
+def server_quit():
+	print('Server shutting down by remote command')
 
-TCP_IP = '192.168.1.78'
+	conn.close()
+	quit()
+
+TCP_IP = '127.0.0.1'
 TCP_PORT = 42626
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 4096  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-while 1:
-	conn, addr = s.accept()
-	print('Connection address:', addr)
-	while 1:
+while 1: #Chasing tails
+	conn, addr = s.accept() #We are now accepting connections
+	print('Connection address:', addr) #Wait, where did that come from??
+	while 1: #Running in circles
 		data = conn.recv(BUFFER_SIZE)
 		if not data: break
-		print("received data:", data)
-		conn.send(data)  # echo
-	conn.close()
+		print("received data:", data.decode('utf-8')) #
+		incoming = data.decode('utf-8')
+		conn.send(data)  # This bit never sends....
+	if incoming == "/exit":
+		shutdown_message = 'Server shutting down'
+		conn.send(shutdown_message.encode('utf-8'))
+		time.sleep(1)
+		server_quit()
+	#And now, CAAAARDS
+	if incoming == "[PlayingCard]":
+		print('playing')
+		cardid = 'CardID'
+		conn.send(cardid.encode('utf-8'))
