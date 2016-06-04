70bfbb53c817de0f7a8b61ecd7d73a53af9ec661
Hans KRC4267 <krc4267@gmail.com>
2016-06-03 22:34:32 -0400

    The client now asks for an IP to connect to, and additionally stays open as a shell-like thing

--- network-client.2.py
+++ network-client.2.py
@@ -2,21 +2,16 @@
 
 import socket
 
-
-<<<<<<< HEAD
-TCP_IP = '192.168.1.78'
+TCP_IP = input('Input the IP to connect to: ')
 TCP_PORT = 42626
-=======
-TCP_IP = '99.38.160.240'
-TCP_PORT = 5005
->>>>>>> 8da939f8291ae1b00d1d8bbb1ec2a861029c77d4
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE.encode('utf-8'))
-data = s.recv(BUFFER_SIZE)
-s.close()
-
-print("received data:", data)
+while 1:
+	MESSAGE = input('Cardgame-0.1>:' )
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE.encode('utf-8'))
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+	print("received data:", data)
